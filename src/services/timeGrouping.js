c52ff4d9c4965c59bf19aaf06edaa0c05f71cf8f
Henry <hlewis@uchicago.edu>
2017-08-14 16:47:11 -0500

    better time groupings

--- src/services/timeGrouping.js
+++ src/services/timeGrouping.js
@@ -1,8 +1,27 @@
 import Timeago from 'timeago.js';
 
+Timeago.register('simplified_local', (number, index) => {
+    return [
+        ['just now', 'right now'],
+        ['just now', 'right now'], //['%s seconds ago', 'in %s seconds'],
+        ['1 minute ago', 'in 1 minute'],
+        [`${Math.ceil(number / 5) * 5} minutes ago`, `in ${Math.ceil(number / 5) * 5} minutes`],
+        ['1 hour ago', 'in 1 hour'],
+        ['%s hours ago', 'in %s hours'],
+        ['1 day ago', 'in 1 day'],
+        ['%s days ago', 'in %s days'],
+        ['1 week ago', 'in 1 week'],
+        ['%s weeks ago', 'in %s weeks'],
+        ['1 month ago', 'in 1 month'],
+        ['%s months ago', 'in %s months'],
+        ['1 year ago', 'in 1 year'],
+        ['%s years ago', 'in %s years']
+    ][index];
+});
+
 class TimeGrouping {
     constructor (relativeTime) {
-        this.timeago = new Timeago(relativeTime);
+        this.timeago = new Timeago(relativeTime, 'simplified_local');
     }
 
     static createGrouping (tabs) {
